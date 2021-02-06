bde7a6e5c2de195a2151ee9a1d2800629d7a849d
DurnevCode <durnev.nsk@yandex.ru>
2021-02-07 01:25:13 +0700

    sqlalchemy + sqlite,  test F

--- app.py
+++ app.py
@@ -1,51 +1,104 @@
 from  flask import Flask, jsonify, request
+import sqlalchemy as db
+from  sqlalchemy.orm import sessionmaker, scoped_session
+from  sqlalchemy import create_engine
+from  sqlalchemy.ext.declarative import declarative_base
+
 
 
 app = Flask(__name__)
 client = app.test_client()
 
+engine = create_engine('sqlite:///db.sqlite')
+
+session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+Base = declarative_base()
+Base.query = session.query_property()
+
+
+from models import *
+
+
+Base.metadata.create_all(bind=engine)
+
+
+
 
 tutorials = [
     {
-        'id': '0'
+        'id': 1,
         'title': 'Video #1. Intro',
         'description': 'GET, POST routes'
         },
         {
-        'id': '1'
+        'id': 2,
         'title': 'Video #2. More features',
-        'description': 'PUT, DELETE routes'   
+        'description': 'PUT, DELETE routes'
         }
 ]
 
 @app.route('/tutorials', methods=['GET'])
 def get_list():
-    return jsonify(tutorials)
+    videos = Video.query.all()
+    serialized = []
+    for video in videos:
+        serialized.append({
+            'id': video.id,
+            'name': video.name,
+            'description': video.description
+        })
+    return jsonify(serialized)
 
 
 @app.route('/tutorials', methods=['POST'])
 def update_list():
-    new_one = request.json
-    tutorials.append(new_one)
-    return jsonify(tutorials)
+    new_one = Video(**request.json)
+    session.add(new_one)
+    session.commit()
+    serialized = ({
+            'id': new_one.id,
+            'name': new_one.name,
+            'description': new_one.description
+        })
+    return jsonify(serialized)
 
 
-@app.route('/tutorials/<int.tutorial_id>', methods=['PUT'])
-def update_tutorials(tutorial_id):
-    item = next((x for x in tutorials if x['id'] == tutorial_id), None)
+@app.route('/tutorials/<int:tutorial_id>', methods=['PUT'])
+def update_tutorial(tutorial_id):
+    item = Video.query.filter(Video.id == tutorial_id).first()
     params = request.json
-    if  not item:
-        return {'mesege': 'No tutorials with this id'}, 400
-    item.update(params)
-    tutorials.append(new_one)
-    return item
-    
+    if not item:
+        return {'massage': 'No tutorials with this id'}, 400
+    for key, value in params.items():
+        setattr(item, key, value)
+        session.commit()
+    serialized = ({
+            'id': item.id,
+            'name': item.name,
+            'description': item.description
+        })
+    return jsonify(serialized)
 
-@app.route('/tutorials/<int.tutorial_id>', methods=['DELETE'])
+
+
+
+@app.route('/tutorials/<int:tutorial_id>', methods=['DELETE'])
 def delete_tutorial(tutorial_id):
-    idx, _  = next((x for x in enumerate(tutorials) if x[1]['id'] == tutorial_id),(None, None))
+    item = Video.query.filter(Video.id == tutorial_id).first()
+    if not item:
+        return {'massage': 'No tutorials with this id'}, 400
+    session.delete(item)
+    session.commit()
+
     tutorials.pop(idx)
     return '', 204
+
+
+@app.teardown_appcontext
+def shutdoun_session(exception=None):
+    session.remove()
+
 
 
 
---
+++ test.py
@@ -0,0 +1,36 @@
+from app import client
+from models import Video
+
+
+
+def test_get():
+    res = client.get('/tutorials')
+
+    assert res.status_code == 200
+    assert len(res.get_json()) == len(Video.query.all())
+    assert res.get_json()[0]['id'] == 1
+
+
+def test_post():
+    data = {
+        'id': 3,
+        'title': 'Video #3. Unit testing',
+        'description': 'Pytest'
+    }
+    res = client.post('/tutorials', json=data)
+
+    assert res.status_code == 200
+    assert res.get_json()['name'] == data['name']
+
+def test_put():
+    res = client.put('/tutorials/1', json={'name': 'UPD'})
+    
+    assert res.status_code == 200
+    assert Video.query.get(1).name == 'UPD'
+
+
+def test_delete():
+    res = client.delete('/tutorials/1')
+    
+    assert res.status_code == 204
+    assert Video.query.get(1) is None
